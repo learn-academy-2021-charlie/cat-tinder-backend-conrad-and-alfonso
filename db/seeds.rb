1788157ac1e77b365d2cbecbba06f02b0081ff84
charper95 <charper1995@gmail.com>
2021-08-25 14:30:40 -0700

    adds cors gem and config

--- db/seeds.rb
+++ db/seeds.rb
@@ -29,10 +29,23 @@ rocks = [
     color: "brown",
     brilliance: 10,
     hardness: 10,
+  },
+  {
+    name: "Serpentine",
+    color: "green",
+    brilliance: 2,
+    hardness: 2,
   }
 ]
 
 rocks.each do |attributes|
-    Rock.create attributes
-    puts "creating rock #{attributes}"
+    new_rock = Rock.new attributes
+    found_rock = Rock.where attributes 
+        if found_rock.length > 0 
+            puts "Found rock with #{attributes}. Not creating new rock."
+        else
+            puts "Rock not found. Creating rock #{attributes}"
+            new_rock.save
+        end
+    # puts "creating rock #{attributes}"
 end
