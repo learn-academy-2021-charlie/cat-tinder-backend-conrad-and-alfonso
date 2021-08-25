6fe2022832c07825525602d977e487be6b1a5de8
charper95 <charper1995@gmail.com>
2021-08-25 14:13:06 -0700

    adds seeds file data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,38 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+rocks = [
+  {
+    name: "Emerald",
+    color: "green",
+    brilliance: 6,
+    hardness: 8,
+  },
+  {
+    name: "Diamond",
+    color: "clear",
+    brilliance: 7,
+    hardness: 10,
+  },
+  {
+    name: "Obsidian",
+    color: "black",
+    brilliance: 3,
+    hardness: 4,
+  },
+  {
+
+    name: "Basalt",
+    color: "grey",
+    brilliance: 0,
+    hardness: 6,
+  },
+  {
+    name: "Dwayne 'The Rock' Johnson",
+    color: "brown",
+    brilliance: 10,
+    hardness: 10,
+  }
+]
+
+rocks.each do |attributes|
+    Rock.create attributes
+    puts "creating rock #{attributes}"
+end
