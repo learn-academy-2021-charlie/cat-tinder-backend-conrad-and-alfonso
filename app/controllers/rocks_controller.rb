54efbf479dae071cf85a45b12b72adf527e20d68
charper95 <charper1995@gmail.com>
2021-08-26 10:50:17 -0700

    adds endpoints and endpoint tests for CRUD

--- app/controllers/rocks_controller.rb
+++ app/controllers/rocks_controller.rb
@@ -1,2 +1,30 @@
 class RocksController < ApplicationController
+
+    def index
+        rocks = Rock.all 
+        render json: rocks
+    end
+
+    def create
+        rock = Rock.create(rock_params)
+        render json: rock
+    end
+
+    def update
+        rock = Rock.find(params[:id])
+        rock.update(rock_params)
+        render json: rock
+    end
+
+    def destroy
+        rock = Rock.find(params[:id])
+        rock.delete()
+        render json: rock
+    end
+
+    private
+    def rock_params
+        params.require(:rock).permit(:name, :color, :brilliance, :hardness)
+    end
+
 end
--- spec/requests/rocks_spec.rb
+++ spec/requests/rocks_spec.rb
@@ -2,6 +2,81 @@ require 'rails_helper'
 
 RSpec.describe "Rocks", type: :request do
   describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+    it 'gets a list of rocks' do
+      Rock.create(
+      name: "Obsidian",
+      color: "black",
+      brilliance: 3,
+      hardness: 4
+      )
+      get '/rocks'
+      rock = JSON.parse(response.body)
+      expect(response).to have_http_status(200)
+      expect(rock.length).to eq 1
+    end
+  end
+
+  describe "POST /create" do
+    it 'creates a rock' do
+      rock_params = {
+        rock: {
+          name: "Obsidian",
+          color: "black",
+          brilliance: 3,
+          hardness: 4,
+        }
+      }
+      post '/rocks', params: rock_params
+      new_rock = Rock.first
+      expect(new_rock.name).to eq 'Obsidian'
+      expect(new_rock.color).to eq 'black'
+      expect(new_rock.brilliance).to eq 3
+      expect(new_rock.hardness).to eq 4
+    end
+  end
+
+  describe "PATCH /update" do
+    it 'updates a rock' do
+      rock_params = {
+        rock: {
+          name: "Obsidian",
+          color: "black",
+          brilliance: 3,
+          hardness: 4,
+        }
+      }
+      # post '/rocks', params: rock_params
+      rock = Rock.create(rock_params[:rock])
+      updated_rock_params = {
+        rock: {
+          name: "Obsidian",
+          color: "black",
+          brilliance: 9,
+          hardness: 4,
+        }
+      }
+      # rock = Rock.first
+      patch "/rocks/#{rock.id}", params: updated_rock_params
+      updated_rock = Rock.first
+      expect(response).to have_http_status(200)
+      expect(updated_rock.brilliance).to eq 9
+    end
+  end
+
+  describe "DELETE /destroy" do
+    it 'deletes a rock' do
+      rock_params = {
+        rock: {
+          name: "Obsidian",
+          color: "black",
+          brilliance: 3,
+          hardness: 4,
+        }
+      }
+    rock = Rock.create(rock_params[:rock])
+    delete "/rocks/#{rock.id}"
+    expect(response).to have_http_status(200)
+    expect(Rock.all.length).to eq 0
+    end
   end
 end
